14f39a59453cf04976c456f3ebc4b1ddb8493100
Eduardo Nitz <dadonitz@gmail.com>
2026-09-04 15:28:18 -0300

    feat: add Kuromi workout art and playful effects

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='treino-fofo-v7';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='treino-fofo-v8';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon-192.png','./assets/icon-512.png','./assets/kuromi-dumbbells.webp','./assets/kuromi-squat.webp','./assets/kuromi-victory.webp'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
